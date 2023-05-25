ab33e24e7d8a74dd516afe93db7ef8e59ab80bfe
Tommy Wiffler <thomas.wiffler@gmail.com>
2023-05-25 15:32:14 -0700

    removed extra comments

--- pkg/utils/response.go
+++ pkg/utils/response.go
@@ -13,7 +13,6 @@ type Error struct {
 	Error string `json:"error"`
 }
 
-// JSON converts data object to JSON and encodes onto http.responseWriter
 func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
 	w.WriteHeader(statusCode)
 	log.Infoln("response.JSON returned " + strconv.Itoa(statusCode))
@@ -24,7 +23,6 @@ func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
 	}
 }
 
-// ERROR converts error into JSON format
 func ERROR(w http.ResponseWriter, statusCode int, err error) {
 	if err != nil {
 		log.Errorln(err.Error())
